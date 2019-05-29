35bcf665cf46860bce74cf95830dc69b52e5296c
kei01138 <choikm3847@gmail.com>
2019-05-29 16:36:57 +0900

    7th hour

--- mysite/app.py
+++ mysite/app.py
@@ -16,16 +16,29 @@ def receive():
   message = request.args.get('message') # => 'hello'
   return render_template('receive.html', user=user, message=message)
 
+@app.route('/lotto_check')
+def lotto_check():
+  return render_template('lotto_check.html')
+
 @app.route('/lotto_result')
 def lotto_result():
-  url = "https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=844"
+  lotto_round = request.args.get('lotto_round')
+  url = f"https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={lotto_round}"
   response = requests.get(url)
   # response.text # => string
   lotto = response.json() # => dict
   winner = []
-  for n in range(1, 7):
-    winner.append(lotto[f'drwtNo{n}'])
-  return render_template('lotto_result.html', lotto=winner)
+  # for n in range(1, 7):
+  #   winner.append(lotto[f'drwtNo{n}'])
+  
+  # list comprehension
+  # a = [n*2 for n in range(1, 7)] # => [2, 4, 6, 8, 10, 12]
+  a = [lotto[f'drwtNo{n}'] for n in range(1, 7)]
+  bonus = lotto['bnusNo']
+  winner = f'{a} + {bonus}'
+  return render_template('lotto_result.html', lotto=winner, bonus=bonus)
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
